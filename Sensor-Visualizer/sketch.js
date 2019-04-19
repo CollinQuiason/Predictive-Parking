f087bbed8f16f8c82e4bd8857a367e081f2d60b7
CollinQuiason <collinqrott@yahoo.com>
2019-04-19 11:12:44 -0500

    attempted changes to detect sensor merge

--- Sensor-Visualizer/sketch.js
+++ Sensor-Visualizer/sketch.js
@@ -15,6 +15,9 @@ let pos;
 var maxparking = [10, 9, 0, 7, 5, 11, 11, 17, 20, 11, 8, 8, 0, 15, 4, 0, 1, 8, 4, 0, 4, 9, 12]; //Index = sensor# - 1
 var predictions = [23];
 var thecount = 0;
+var nSensors;
+var centX, centY, maxX, maxY, minX, minY;
+var pixelMarginThreshold = 50; //Maximum pixels required to merge sensor predictions visually
 
 
 
@@ -31,15 +34,38 @@ const options = {
 
 //Called before HTML5 canvas loads
 function preload(){
-   data = loadTable('/KansasCityData/sensorInformation.csv', 'csv', 'header');
+  data = loadTable('/KansasCityData/sensorInformation.csv', 'csv', 'header');
   predictions[13] = loadTable('/KansasCityData/TimeSeries_Sensor14.csv', 'csv', 'header');
     //TODO: Sort via 'start_time' (Might be pre-processed)
 }
 
 //Called after HTML5 canvas loads
 function setup(){
-  
 
+  //Get all sensor bounds
+
+  nSensors = data.getRowCount();
+  centX = [nSensors];
+  centY = [nSensors];
+  maxX = [nSensors];
+  maxY = [nSensors];
+  minX = [nSensors];
+  minY = [nSensors];
+
+  for (var i = 0; i < data.getRowCount(); i++){
+
+    //Centroid
+    centX[i] = parseFloat(data.getRow(i).getString('centroidX'));
+    centY[i] = parseFloat(data.getRow(i).getString('centroidY'));
+
+    //Bounds
+    maxX[i] = parseFloat(data.getRow(i).getString('maxX'));
+    maxY[i] = parseFloat(data.getRow(i).getString('maxY'));
+    minX[i] = parseFloat(data.getRow(i).getString('minX'));
+    minY[i] = parseFloat(data.getRow(i).getString('minY'));
+
+}
+  //frameRate(15);
 
   //Canvas setup
   h = window.innerHeight;
@@ -59,70 +85,88 @@ function setup(){
 
 //Called every frame
 function draw(){
-  print(thecount);
+  //console.log(thecount);
+  //console.log(frameRate());
   drawSensors();
 }
 
 function drawSensors(){
   clear();//Otherwise it looks like a crashing windows program where each frame doesn't clear
-
+  var centroid = [nSensors];
+  var maxCoord = [nSensors];
+  var minCoord = [nSensors];
   //For each sensor
-  for (var i = 0; i < data.getRowCount(); i++){
 
-    //Centroid
-    var centX = parseFloat(data.getRow(i).getString('centroidX'));
-    var centY = parseFloat(data.getRow(i).getString('centroidY'));
-
-    //Bounds
-    var maxX = parseFloat(data.getRow(i).getString('maxX'));
-    var maxY = parseFloat(data.getRow(i).getString('maxY'));
-    var minX = parseFloat(data.getRow(i).getString('minX'));
-    var minY = parseFloat(data.getRow(i).getString('minY'));
+  for (var i = 0; i < nSensors; i++){
 
     //Draw the ellipse
-    if (centX != 0 || centY != 0){ //If the sensor contains parking data
+    if (centX[i] != 0 || centY[i] != 0){ //If the sensor contains parking data
 
-      //Parking Prediction for sensor: 'i'
-      //var availability = Math.random();
-      print(thecount);
+
       var availability = parseFloat(predictions[13].getRow(thecount).getString('no_of_cars'))/maxparking[13]; 
       var availabilityRGB = [255*availability, 255*(1-availability), 0];
       //Convert lat/longitude to pixels on the screen
-      var centroid = areaMap.latLngToPixel(centY, centX); //TODO change csv file so that these aren't reverse X and Y
-      var maxCoord = areaMap.latLngToPixel(maxY, maxX);
-      var minCoord = areaMap.latLngToPixel(minY, minX);
-
-      //Actually draw the ellipse
-      //stroke(255, 0, 0, 50);
-      /*
-      fill(255);
-      ellipse(centroid.x, centroid.y, 5, 5);
-      fill(255, 100, 255);
-      ellipse(maxCoord.x, maxCoord.y, 5, 5);
-      fill(100, 255, 255);
-      ellipse(minCoord.x, minCoord.y, 5, 5);
-      */
+      centroid[i] = areaMap.latLngToPixel(centY[i], centX[i]); //TODO change csv file so that these aren't reverse X and Y
+      maxCoord[i] = areaMap.latLngToPixel(maxY[i], maxX[i]);
+      minCoord[i] = areaMap.latLngToPixel(minY[i], minX[i]);
 
       //Polygon around sensor area
-      noStroke();
-      fill(availabilityRGB[0], availabilityRGB[1], availabilityRGB[2], 110);
-      beginShape();
 
-      curveVertex(minCoord.x, minCoord.y);
-      curveVertex(maxCoord.x, minCoord.y);
-      curveVertex(maxCoord.x, maxCoord.y);
-      curveVertex(minCoord.x, maxCoord.y);
-      curveVertex(minCoord.x, minCoord.y);
-      curveVertex(maxCoord.x, minCoord.y);
+      /*
 
-
-      endShape(CLOSE);
-
+      */
     }
     else{
       //console.log("Sensor number: -" + i + "- is null. No parking data for that sensor.");
     }
   }
+  //console.log(maxCoord[10].x);
+  for (var i = 0; i < nSensors - 1; i++){
+    if (centX[i] != 0 || centY[i] != 0){
+      for (var j = i + 1; j < nSensors; j++){
+        if (centX[j] != 0 || centY[j] != 0){
+          if (Math.abs(maxCoord[i].x - maxCoord[j].x) <= pixelMarginThreshold ||
+                Math.abs(maxCoord[i].y - maxCoord[j].x) <= pixelMarginThreshold ||
+                Math.abs(minCoord[i].x - maxCoord[j].x) <= pixelMarginThreshold ||
+                Math.abs(minCoord[i].y - maxCoord[j].x) <= pixelMarginThreshold ||
+                Math.abs(maxCoord[i].x - maxCoord[j].y) <= pixelMarginThreshold ||
+                Math.abs(maxCoord[i].y - maxCoord[j].y) <= pixelMarginThreshold ||
+                Math.abs(minCoord[i].x - maxCoord[j].y) <= pixelMarginThreshold ||
+                Math.abs(minCoord[i].y - maxCoord[j].y) <= pixelMarginThreshold ||
+                Math.abs(maxCoord[i].x - minCoord[j].x) <= pixelMarginThreshold ||
+                Math.abs(maxCoord[i].y - minCoord[j].x) <= pixelMarginThreshold ||
+                Math.abs(minCoord[i].x - minCoord[j].x) <= pixelMarginThreshold ||
+                Math.abs(minCoord[i].y - minCoord[j].x) <= pixelMarginThreshold ||
+                Math.abs(maxCoord[i].x - minCoord[j].y) <= pixelMarginThreshold ||
+                Math.abs(maxCoord[i].y - minCoord[j].y) <= pixelMarginThreshold ||
+                Math.abs(minCoord[i].x - minCoord[j].y) <= pixelMarginThreshold ||
+                Math.abs(minCoord[i].y - minCoord[j].y) <= pixelMarginThreshold
+                ){
+            console.log(i + " " + j);
+          }
+      }
+      }
+  }
+  }
+
+  for (var i = 0; i < nSensors; i++){
+    if (centX[i] != 0 || centY[i] != 0){
+      noStroke();
+      fill(availabilityRGB[0], availabilityRGB[1], availabilityRGB[2], 110);
+      beginShape();
+
+      curveVertex(minCoord[i].x, minCoord[i].y);
+      curveVertex(maxCoord[i].x, minCoord[i].y);
+      curveVertex(maxCoord[i].x, maxCoord[i].y);
+      curveVertex(minCoord[i].x, maxCoord[i].y);
+      curveVertex(minCoord[i].x, minCoord[i].y);
+      curveVertex(maxCoord[i].x, minCoord[i].y);
+
+
+      endShape(CLOSE);
+    }
+  }
+
   thecount += 1;
   thecount %= predictions[13].getRowCount();
 }
@@ -130,8 +174,6 @@ function drawSensors(){
 function tick(){
 
 }
-
-
 //Adaptively resize window
 function windowResized() {
   //TODO: resize map as well
@@ -139,6 +181,4 @@ function windowResized() {
     h = window.innerHeight;
     resizeCanvas(w, h);
 
-  }
-
-
+}
